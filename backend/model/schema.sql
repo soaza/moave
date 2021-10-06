3f2b29291bb969c2e9a3df057e3ca13c3a35dcf9
Constance <constancensq@gmail.com>
2021-10-06 23:41:17 +0800

    Update schema

--- backend/model/schema.sql
+++ backend/model/schema.sql
@@ -1,6 +1,27 @@
-CREATE TABLE database_user (
+CREATE TABLE User (
     user_id SERIAL,
-    username TEXT UNIQUE,
-    password TEXT,
-    PRIMARY KEY(user_id)
+    display_name TEXT UNIQUE NOT NULL,
+    email TEXT,
+    password TEXT NOT NULL,
+    PRIMARY KEY(email)
+);
+
+CREATE TABLE Follows (
+    follower_id INTEGER references User(user_id),
+    following_id INTEGER references User(user_id),
+    PRIMARY KEY(follower_id, following_id),
+    check (follower_id != following_id)
+);
+
+CREATE TABLE MovieList (
+    user_id INTEGER references User(user_id),
+    list_name TEXT,
+    PRIMARY KEY(user_id, list_name) 
+);
+
+CREATE TABLE Movies (
+    user_id INTEGER NOT NULL references User(user_id),
+    list_name TEXT NOT NULL references MovieList(list_name), 
+    movie_id INTEGER NOT NULL,
+    PRIMARY KEY(user_id, list_name, movie_id)
 );
