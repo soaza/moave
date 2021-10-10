df746e9b5da714c3810c56747f1da9014b201f9f
gordonfgz <gordonfgz@gmail.com>
2021-10-10 16:55:01 +0800

    gordon made changes

--- backend/controllers/user/account.js
+++ backend/controllers/user/account.js
@@ -4,15 +4,16 @@ const accountSignUp = async (request, response, pool) => {
   const body = request.body;
   const username = body.username;
   const password = body.password;
+  const email = body.email;
 
   // generate salt to hash password
   const salt = await bcrypt.genSalt(10);
   // now we set user password to hashed password
   const hashedPassword = await bcrypt.hash(password, salt);
 
-  const query = `INSERT INTO database_user VALUES (DEFAULT,$1,$2)`;
+  const query = `INSERT INTO Users VALUES (DEFAULT,$1,$2,$3)`;
 
-  pool.query(query, [username, hashedPassword], (error, results) => {
+  pool.query(query, [email, hashedPassword, username], (error, results) => {
     if (error) {
       console.log(error);
       response.status(500).json({
@@ -28,14 +29,15 @@ const accountSignUp = async (request, response, pool) => {
 
 const accountLogin = async (request, response, pool) => {
   const params = request.query;
-  const username = params.username;
+  const email = params.email;
   const password = params.password;
 
-  const query = `SELECT password FROM database_user WHERE username = $1`;
+  const query = `SELECT password FROM Users WHERE email = $1`;
 
-  pool.query(query, [username], async (error, results) => {
+  pool.query(query, [email], async (error, results) => {
     if (error || !results.rows[0]) {
       console.log(error);
+      console.log(results.rows[0]);
       response.status(500).json({
         success: false,
       });
--- backend/model/schema.sql
+++ backend/model/schema.sql
@@ -1,26 +1,25 @@
-CREATE TABLE User (
+CREATE TABLE Users (
     user_id SERIAL,
-    display_name TEXT UNIQUE NOT NULL,
-    email TEXT,
+    email TEXT PRIMARY KEY,
     password TEXT NOT NULL,
-    PRIMARY KEY(email)
+    username TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE Follows (
-    follower_id INTEGER references User(user_id),
-    following_id INTEGER references User(user_id),
+    follower_id INTEGER references Users(user_id),
+    following_id INTEGER references Users(user_id),
     PRIMARY KEY(follower_id, following_id),
     check (follower_id != following_id)
 );
 
 CREATE TABLE MovieList (
-    user_id INTEGER references User(user_id),
+    user_id INTEGER references Users(user_id),
     list_name TEXT,
     PRIMARY KEY(user_id, list_name) 
 );
 
 CREATE TABLE Movies (
-    user_id INTEGER NOT NULL references User(user_id),
+    user_id INTEGER NOT NULL references Users(user_id),
     list_name TEXT NOT NULL references MovieList(list_name), 
     movie_id INTEGER NOT NULL,
     PRIMARY KEY(user_id, list_name, movie_id)
