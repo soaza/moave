24003ce60fa83dd9f3b36c9ae2e1282b99ac5603
gordonfgz <gordonfgz@gmail.com>
2021-10-10 18:47:22 +0800

    add apis for follow, search and changePassword *NOTE* login has been changed to a POST request instead of a GET request because it should be done this way due to security

--- backend/controllers/user/account.js
+++ backend/controllers/user/account.js
@@ -27,14 +27,15 @@ const accountSignUp = async (request, response, pool) => {
   });
 };
 
+
 const accountLogin = async (request, response, pool) => {
-  const params = request.query;
-  const email = params.email;
-  const password = params.password;
+  const body = request.body;
+  const username = body.username;
+  const password = body.password;
 
-  const query = `SELECT password FROM Users WHERE email = $1`;
+  const query = `SELECT * FROM Users WHERE username = $1`;
 
-  pool.query(query, [email], async (error, results) => {
+  pool.query(query, [username], async (error, results) => {
     if (error || !results.rows[0]) {
       console.log(error);
       console.log(results.rows[0]);
@@ -47,6 +48,11 @@ const accountLogin = async (request, response, pool) => {
 
       if (validPassword) {
         response.status(200).json({
+          // cant do this yet cos front end doesnt check for user info 
+          // so maybe we just leave the returning to getUserInfo commands?????
+          //user_id: results.rows[0].user_id,
+          //username: results.rows[0].username,
+          //email: results.rows[0].email
           success: true,
         });
       } else {
@@ -58,7 +64,188 @@ const accountLogin = async (request, response, pool) => {
   });
 };
 
+const changePassword = async (request, response, pool) => {
+  const body = request.body;
+  const email = body.email;
+  const oldPassword = body.oldPassword;
+  const newPassword = body.newPassword;
+  console.log(email);
+  // generate salt to hash password
+  const salt = await bcrypt.genSalt(10);
+
+  const query = `SELECT * FROM Users WHERE email = $1`;
+
+  pool.query(query, [email], async (error, results) => {
+    if (error || !results.rows[0] || oldPassword == newPassword) {
+      console.log(error);
+      console.log(results.rows[0]);
+      response.status(500).json({
+        success: false,
+      });
+    } else {
+      // get stored password
+      const hashedPassword = results.rows[0].password;
+      const validPassword = await bcrypt.compare(oldPassword, hashedPassword);
+
+      if (validPassword) {
+        const hashedNewPassword = await bcrypt.hash(newPassword, salt);
+        const query = 'UPDATE USERS SET PASSWORD = $1 WHERE email = $2';
+        pool.query(query, [hashedNewPassword, email], async (error, results) => {
+          response.status(200).json({
+            success: "Password Successfully Changed."
+          });
+
+        });
+      } else {
+        response.status(500).json({
+          success: false,
+        });
+      }
+    }
+  });
+};
+
+
+
+
+
+
+const getUserInfoById = async (request, response, pool) => {
+  const {user_id} = request.params;
+  const query = `SELECT * FROM Users WHERE user_id = $1`;
+  pool.query(query, [user_id], async (error, results) => {
+    if (error || !results.rows[0]) {
+      console.log(error);
+      console.log(results.rows[0]);
+      response.status(500).json({
+        success: false,
+      });
+    } else {
+      response.status(200).json({
+        user_id: results.rows[0].user_id,
+        username: results.rows[0].username,
+        email: results.rows[0].email});
+    }
+  });
+};
+
+const getUserInfoByUsername = async (request, response, pool) => {
+  const {username} = request.params;
+  const query = `SELECT * FROM Users WHERE username = $1`;
+  pool.query(query, [username], async (error, results) => {
+    if (error || !results.rows[0]) {
+      console.log(error);
+      console.log(results.rows[0]);
+      response.status(500).json({
+        success: false,
+      });
+    } else {
+      response.status(200).json({
+        user_id: results.rows[0].user_id,
+        username: results.rows[0].username,
+        email: results.rows[0].email});
+    }
+  });
+};
+
+const getUserInfoByEmail = async (request, response, pool) => {
+  const {email} = request.params;
+  const query = `SELECT * FROM Users WHERE email = $1`;
+  pool.query(query, [email], async (error, results) => {
+    if (error || !results.rows[0]) {
+      console.log(error);
+      console.log(results.rows[0]);
+      response.status(500).json({
+        success: false,
+      });
+    } else {
+      response.status(200).json({
+        user_id: results.rows[0].user_id,
+        username: results.rows[0].username,
+        email: results.rows[0].email});
+    }
+  });
+};
+
+
+
+const followAccount = async (request, response, pool) => {
+  // info is for the person u are tryna follow
+  const body = request.body;
+  const followerId= body.followerId;
+  const followingId = body.followingId
+
+  const query = `INSERT INTO Follows VALUES ($1,$2)`;
+  pool.query(query, [followerId, followingId], (error, results) => {
+    if (error) {
+      console.log(error);
+      response.status(500).json({
+        success: false,
+      });
+    } else {
+      response.status(200).json({
+        success: true,
+      });
+    }
+  });
+}
+
+const getFollowers = async (request, response, pool) => {
+  const {user_id} = request.params;
+  const query = `SELECT user_id, email, username
+  FROM Users
+  WHERE EXISTS (
+      SELECT 1
+      FROM Follows
+      WHERE following_id = $1
+      AND Users.user_id = Follows.follower_id
+  )`;
+  pool.query(query, [user_id], async (error, results) => {
+    if (error) {
+      console.log(error);
+      response.status(500).json({
+        success: false,
+      });
+    } else {
+      response.status(200).json({
+        Followers: results.rows
+      });
+    }
+  });
+};
+
+const getFollowing = async (request, response, pool) => {
+  const {user_id} = request.params;
+  const query = `SELECT user_id, email, username
+  FROM Users
+  WHERE EXISTS (
+      SELECT 1
+      FROM Follows
+      WHERE follower_id = $1
+      AND Users.user_id = Follows.following_id
+  )`;
+  pool.query(query, [user_id], async (error, results) => {
+    if (error) {
+      console.log(error);
+      response.status(500).json({
+        success: false,
+      });
+    } else {
+      response.status(200).json({
+        Following: results.rows
+      });
+    }
+  });
+};
+
 module.exports = {
   accountSignUp,
   accountLogin,
+  changePassword,
+  getUserInfoById,
+  getUserInfoByUsername,
+  getUserInfoByEmail,
+  followAccount,
+  getFollowers,
+  getFollowing
 };
--- backend/model/schema.sql
+++ backend/model/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Users (
-    user_id SERIAL,
-    email TEXT PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
+    email TEXT UNIQUE NOT NULL,
     password TEXT NOT NULL,
     username TEXT UNIQUE NOT NULL
 );
@@ -23,4 +23,13 @@ CREATE TABLE Movies (
     list_name TEXT NOT NULL references MovieList(list_name), 
     movie_id INTEGER NOT NULL,
     PRIMARY KEY(user_id, list_name, movie_id)
+);
+
+SELECT *
+FROM Users
+WHERE EXISTS (
+    SELECT 1
+    FROM Follows
+    WHERE following_id = 1
+    AND Users.user_id = Follows.follower_id
 );
