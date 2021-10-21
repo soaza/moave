194e4c35c03333b15a8dd9a76dbb94f576ea0889
gordonfgz <gordonfgz@gmail.com>
2021-10-21 23:19:11 +0800

    add new apis for activities and groups

---
+++ backend/controllers/activityControllers/activity.js
@@ -0,0 +1,214 @@
+const addMovieToWatchlist = async (request, response, pool) => {
+    const body = request.body;
+    const movie_id = body.movie_id;
+    const user_id = body.user_id;
+  
+    const query = `INSERT INTO ActivityList VALUES ($1,$2,$3)`;
+    pool.query(query, [user_id, movie_id, "WATCHLIST"], (error, results) => {
+      if (error) {
+        console.log(error);
+        response.status(500).json({
+          success: false,
+        });
+      } else {
+        response.status(200).json({
+          success: `Added Movie:${movie_id} to Watchlist.`,
+        });
+      }
+    });
+  };
+
+  const addMovieToCompleted = async (request, response, pool) => {
+    const body = request.body;
+    const movie_id = body.movie_id;
+    const user_id = body.user_id;
+  
+    const query = `INSERT INTO ActivityList VALUES ($1,$2,$3)`;
+    pool.query(query, [user_id, movie_id, "COMPLETED"], (error, results) => {
+      if (error) {
+        console.log(error);
+        response.status(500).json({
+          success: false,
+        });
+      } else {
+        response.status(200).json({
+          success: `Added Movie:${movie_id} to Completed.`,
+        });
+      }
+    });
+  };
+
+  const addMovieToCurrentlyWatching = async (request, response, pool) => {
+    const body = request.body;
+    const movie_id = body.movie_id;
+    const user_id = body.user_id;
+  
+    const query = `INSERT INTO ActivityList VALUES ($1,$2,$3)`;
+    pool.query(query, [user_id, movie_id, "CURRENT"], (error, results) => {
+      if (error) {
+        console.log(error);
+        response.status(500).json({
+          success: false,
+        });
+      } else {
+        response.status(200).json({
+          success: `Added Movie:${movie_id} to Currently Watching.`,
+        });
+      }
+    });
+  };
+
+  const updateMovieToCompleted = async (request, response, pool) => {
+    const body = request.body;
+    const movie_id = body.movie_id;
+    const user_id = body.user_id;
+    
+    const query = `UPDATE ActivityList SET activity_type = $3 WHERE user_id = $1 AND movie_id = $2`;
+    pool.query(query, [user_id, movie_id, "COMPLETED"], (error, results) => {
+      if (error) {
+        console.log(error);
+        response.status(500).json({
+          success: false,
+        });
+      } else {
+        response.status(200).json({
+          success: `Movie:${movie_id} updated to Completed for User:${user_id}.`,
+        });
+      }
+    });
+  };
+
+  const updateMovieToWatchlist = async (request, response, pool) => {
+    const body = request.body;
+    const movie_id = body.movie_id;
+    const user_id = body.user_id;
+    
+    const query = `UPDATE ActivityList SET activity_type = $3 WHERE user_id = $1 AND movie_id = $2`;
+    pool.query(query, [user_id, movie_id, "WATCHLIST"], (error, results) => {
+      if (error) {
+        console.log(error);
+        response.status(500).json({
+          success: false,
+        });
+      } else {
+        response.status(200).json({
+          success: `Movie:${movie_id} updated to Completed for User:${user_id}.`,
+        });
+      }
+    });
+  };
+
+  const updateMovieToCurrentlyWatching = async (request, response, pool) => {
+    const body = request.body;
+    const movie_id = body.movie_id;
+    const user_id = body.user_id;
+    
+    const query = `UPDATE ActivityList SET activity_type = $3 WHERE user_id = $1 AND movie_id = $2`;
+    pool.query(query, [user_id, movie_id, "CURRENT"], (error, results) => {
+      if (error) {
+        console.log(error);
+        response.status(500).json({
+          success: false,
+        });
+      } else {
+        response.status(200).json({
+          success: `Movie:${movie_id} updated to Completed for User:${user_id}.`,
+        });
+      }
+    });
+  };
+
+  const dropMovie = async (request, response, pool) => {
+    const body = request.body;
+    const user_id = body.user_id;    
+    const movie_id = body.movie_id;  
+  
+    const query = `DELETE FROM ActivityList WHERE user_id = $1 AND movie_id = $2`;
+    pool.query(query, [user_id, movie_id], (error, results) => {
+        if (error) {
+        console.log(error);
+        response.status(500).json({
+            Error: "Cannot drop movie idk why LOL",
+        });
+        } else {
+        response.status(200).json({
+            Success: `Movie:${movie_id} dropped for User:${user_id}`,
+        });
+        }
+    });
+  };
+
+  const getCompleted = async (request, response, pool) => {
+    const { user_id } = request.params;
+    const query = `SELECT * FROM ActivityList WHERE user_id = $1 AND activity_type = $2`;
+    pool.query(query, [user_id, "COMPLETED"], async (error, results) => {
+        if (error || !results.rows[0]) {
+        console.log(error);
+        console.log(results.rows[0]);
+        response.status(500).json({
+            success: false,
+        });
+        } else {
+            response.status(200).json({
+                data: results.rows,
+                size: results.rows.length,
+            });
+        }
+    });
+  };
+
+  const getWatchlist = async (request, response, pool) => {
+    const { user_id } = request.params;
+    const query = `SELECT * FROM ActivityList WHERE user_id = $1 AND activity_type = $2`;
+    pool.query(query, [user_id, "WATCHLIST"], async (error, results) => {
+        if (error || !results.rows[0]) {
+        console.log(error);
+        console.log(results.rows[0]);
+        response.status(500).json({
+            success: false,
+        });
+        } else {
+            response.status(200).json({
+                data: results.rows,
+                size: results.rows.length,
+            });
+        }
+    });
+  };
+
+  const getCurrentlyWatching = async (request, response, pool) => {
+    const { user_id } = request.params;
+    const query = `SELECT * FROM ActivityList WHERE user_id = $1 AND activity_type = $2`;
+    pool.query(query, [user_id, "CURRENT"], async (error, results) => {
+        if (error || !results.rows[0]) {
+        console.log(error);
+        console.log(results.rows[0]);
+        response.status(500).json({
+            success: false,
+        });
+        } else {
+            response.status(200).json({
+                data: results.rows,
+                size: results.rows.length,
+            });
+        }
+    });
+  };
+
+
+
+
+
+
+module.exports = {
+    addMovieToWatchlist,
+    addMovieToCompleted,
+    addMovieToCurrentlyWatching,
+    updateMovieToCompleted,
+    updateMovieToWatchlist,
+    updateMovieToCurrentlyWatching,
+    dropMovie,
+    getCompleted,
+    getWatchlist,
+    getCurrentlyWatching
+};
--- backend/model/schema.sql
+++ backend/model/schema.sql
@@ -26,6 +26,57 @@ CREATE TABLE Movies (
     PRIMARY KEY(user_id, list_name, movie_id)
 );
 
+CREATE TABLE Groups (
+    group_id SERIAL PRIMARY KEY,
+    group_name TEXT UNIQUE NOT NULL,
+    admin_id INTEGER references Users(user_id)
+);
+
+CREATE TABLE UserGroups (
+    user_id INTEGER references Users(user_id),
+    group_id INTEGER references Groups(group_id),
+    PRIMARY KEY(user_id, group_id)
+);
+
+-- lists for activity
+CREATE TABLE ActivityList(
+    user_id INTEGER references Users(user_id),
+    movie_id INTEGER NOT NULL,
+    activity_type TEXT NOT NULL,
+    check (activity_type in ('COMPLETED', 'WATCHLIST', 'CURRENT')),
+    PRIMARY KEY (user_id, movie_id)
+);
+
+
+-- trigger and function set for group creation
+CREATE TRIGGER userGroups_create_trigger
+AFTER INSERT ON Groups
+FOR EACH ROW EXECUTE PROCEDURE update_userGroups_on_create_func();
+
+CREATE OR REPLACE FUNCTION update_userGroups_on_create_func() RETURNS TRIGGER
+AS $$ 
+BEGIN
+    INSERT INTO UserGroups(user_id, group_id) VALUES (NEW.admin_id, NEW.group_id);
+    RETURN NULL; 
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- trigger and function set for group deletion
+CREATE TRIGGER userGroups_delete_trigger
+BEFORE DELETE ON Groups
+FOR EACH ROW EXECUTE PROCEDURE update_userGroups_on_delete_func();
+
+CREATE OR REPLACE FUNCTION update_userGroups_on_delete_func() RETURNS TRIGGER
+AS $$ 
+BEGIN
+    DELETE FROM UserGroups WHERE group_id = OLD.group_id;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
 
 INSERT INTO Users values (DEFAULT,'user1@gmail.com', '$2b$10$XShItmRbeoeCEtK0chNZLuRam3y.k6BRz8hKdKMfdLSWu2NMULZvC', 'user1'); /*pw = user1 */
 INSERT INTO Users values (DEFAULT,'user2@gmail.com', '$2b$10$99g1brQVF8gXamaFtLe5MeOMC.DWefDfRaDjbXiqdU8mEkhnAAuYy', 'user2'); /*pw = user2 */
