117474111ce4d1298b724d43674ec81368d18665
soaza <65228562+soaza@users.noreply.github.com>
2021-11-08 21:46:47 +0800

    Add db url

--- frontend/src/common/api.tsx
+++ frontend/src/common/api.tsx
@@ -86,7 +86,8 @@ async function post<T>(request: IRequest): Promise<T> {
 
 // API Calls
 
-const BASE_URL = "http://localhost:3002";
+// const BASE_URL = "http://localhost:3002";
+const BASE_URL = "https://boiling-citadel-87511.herokuapp.com";
 
 // Account
 export const registerUser = async (username: string, password: string) => {
@@ -109,7 +110,11 @@ export const loginUser = async (username: string, password: string) => {
   });
 };
 
-export const changePassword = async (username: string, oldPassword: string, newPassword: string) => {
+export const changePassword = async (
+  username: string,
+  oldPassword: string,
+  newPassword: string
+) => {
   return patch<{ user_id: string; success: boolean }>({
     endpoint: `${BASE_URL}/changePassword`,
     data: {
